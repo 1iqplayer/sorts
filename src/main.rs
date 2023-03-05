7814786d2d59b3853528254fc5332c7d55cce1e3
Your Name <you@example.com>
2023-03-05 18:30:37 +0100

    sort time measure

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use std::io;
 use std::vec;
 use rand::Rng;
+use std::time;
 
 fn main() {
-    let mut buff = String::new();
-    io::stdin()
-        .read_line(&mut buff)
-        .expect("failed to read line!");
-    let num: usize = buff.trim().parse().expect("input is not a number!");
-    println!("generating random array of {} elements", num);
-
-    let mut i = 0;
+    
+    let n = parse_int("Enter no elements: ");
+    let max = parse_int("Enter max size of element: ");
+    
+    print!("generating random array of {} elements... ", n);
+    let mut now = time::Instant::now();
     let mut v: Vec<usize> = vec::Vec::new();
-    while i<num{
-        v.push(rand::thread_rng().gen_range(0..=20));
-        i += 1;
+    for _ in 0..n{
+        v.push(rand::thread_rng().gen_range(0..=max));
     }   
-    println!("Before: {:?}", &v);
-    quicksort(v.as_mut_slice());
-    println!("after:  {:?}", &v);
+    print!("Done in {}ms", now.elapsed().as_millis());
+
+    print!("Sorting bubble...");
+    let mut fresh = v.to_vec();
+    now = time::Instant::now();
+    sort_bubble(fresh.as_mut_slice());
+    println!(" Done in {}ms", now.elapsed().as_millis());
+
+    print!("Sorting bubblev2...");
+    let mut fresh = v.to_vec();
+    now = time::Instant::now();
+    sort_bubblev2(fresh.as_mut_slice());
+    println!(" Done in {}ms", now.elapsed().as_millis());
+
+    print!("Sorting quick...");
+    let mut fresh = v.to_vec();
+    now = time::Instant::now();
+    sort_quick(fresh.as_mut_slice());
+    println!(" Done in {}ms", now.elapsed().as_millis());
 }
-fn quicksort<T: Ord + std::fmt::Debug>(sl: &mut [T]){
+fn sort_quick<T: Ord + std::fmt::Debug>(sl: &mut [T]){
     let sl_len = sl.len();
     _quicksort(sl, 0, sl_len-1)
 }
@@ -45,4 +59,40 @@ fn partition<T: Ord>(sl: &mut [T], low: usize, high: usize) -> usize{
         }
     }
     is-1
+}
+
+fn sort_bubble<T: Ord>(sl: &mut [T]){
+    let mut i = sl.len()-1;
+    while i > 1{
+        let mut swap = 0;
+        let mut seq = true;
+        for j in 1..=i{
+            if sl[j] > sl[swap]{
+                swap = j;
+            }else{seq = false}
+        }
+        if seq == true {break}
+        sl.swap(swap, i);
+        i -= 1;
+    }
+}
+
+fn sort_bubblev2<T: Ord>(sl: &mut [T]){
+    let n = sl.len();
+    for i in 0..n-1{
+        for j in 0..n-i-1{
+            if sl[j]>sl[j+1] {sl.swap(j, j+1)}
+        }
+    }
+}
+
+fn parse_int(msg: &str)->usize{
+    print!("\n{}", msg);
+    io::Write::flush(&mut io::stdout()).expect("flush failed!");
+    let mut buff = String::new();
+    io::stdin()
+        .read_line(&mut buff)
+        .expect("failed to read from input!");
+    let num: usize = buff.trim().parse().expect("invalid number!");
+    num
 }
