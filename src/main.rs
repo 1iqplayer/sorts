1aa5aaa16e8a1d7200e36a1e17fb1d3ca9514ab0
Olek <uchihaolek@gmail.com>
2023-03-06 21:35:39 +0100

    packed in struct, need to do tests

--- src/main.rs
+++ src/main.rs
@@ -7,83 +7,169 @@ fn main() {
     
     let n = parse_int("Enter no elements: ");
     let max = parse_int("Enter max size of element: ");
+    let inc = parse_int("Sort increasing? yes-1 no-0") != 0;
     
-    print!("generating random array of {} elements... ", n);
-    let mut now = time::Instant::now();
-    let mut v: Vec<usize> = vec::Vec::new();
-    for _ in 0..n{
-        v.push(rand::thread_rng().gen_range(0..=max));
-    }   
-    print!("Done in {}ms", now.elapsed().as_millis());
+    let mut sort = sortx{
+        arr_size: n,
+        arr_range: max,
+        arr: Vec::new(),
+        dec: !inc
+    };
 
-    print!("Sorting bubble...");
-    let mut fresh = v.to_vec();
-    now = time::Instant::now();
-    sort_bubble(fresh.as_mut_slice());
-    println!(" Done in {}ms", now.elapsed().as_millis());
-
-    print!("Sorting bubblev2...");
-    let mut fresh = v.to_vec();
-    now = time::Instant::now();
-    sort_bubblev2(fresh.as_mut_slice());
-    println!(" Done in {}ms", now.elapsed().as_millis());
-
-    print!("Sorting quick...");
-    let mut fresh = v.to_vec();
-    now = time::Instant::now();
-    sort_quick(fresh.as_mut_slice());
-    println!(" Done in {}ms", now.elapsed().as_millis());
-}
-fn sort_quick<T: Ord + std::fmt::Debug>(sl: &mut [T]){
-    let sl_len = sl.len();
-    _quicksort(sl, 0, sl_len-1)
+    sort.arr_gen();
+    //sort.sort_quick();
+    sort.sort_bubble();
+    //sort.sort_bubblev2();
 }
 
-fn _quicksort<T: Ord + std::fmt::Debug>(sl: &mut [T], low: usize, high: usize){
-    let p = partition(sl, low, high);
-    if low != p {_quicksort(sl, low, p-1);}
-    if p != high {_quicksort(sl, p+1, high);}
+impl sortx {
+    fn sort_quick(&self){
+        let mut cpy = self.arr.to_vec();
+        self._quicksort(&mut cpy, 0, self.arr_size-1);
 
-}
+        if self.dec  {self.dec_test(&mut cpy);}
+        else    {self.inc_test(&mut cpy);}
+        println!("{:?}", cpy);
+    }
 
-fn partition<T: Ord>(sl: &mut [T], low: usize, high: usize) -> usize{
-    let mut ii = low;
-    let mut is = low;
-    for _i in low..=high{
-        ii += 1;
-        if sl[ii-1] <= sl[high]{
-            is += 1;
-            if ii != is{
-                sl.swap(ii-1, is-1)
+    fn _quicksort(&self, v: &mut Vec<usize>, low: usize, high: usize){
+        let p = self.partition(v, low, high);
+        if low != p {self._quicksort(v, low, p-1);}
+        if p != high {self._quicksort(v, p+1, high);}
+    }
+
+    fn partition(&self, v: &mut Vec<usize>, low: usize, high: usize) -> usize{
+        let mut ii = low;
+        let mut is = low;
+        if self.dec{
+
+            for _i in low..=high{
+                ii += 1;
+                if v[ii-1] >= v[high]{
+                    is += 1;
+                    if ii != is{
+                        v.swap(ii-1, is-1)
+                    }
+                }
+            }
+        }else{
+            for _i in low..=high{
+                ii += 1;
+                if v[ii-1] <= v[high]{
+                    is += 1;
+                    if ii != is{
+                        v.swap(ii-1, is-1)
+                    }
+                }
             }
         }
+        is-1
     }
-    is-1
-}
 
-fn sort_bubble<T: Ord>(sl: &mut [T]){
-    let mut i = sl.len()-1;
-    while i > 1{
-        let mut swap = 0;
-        let mut seq = true;
-        for j in 1..=i{
-            if sl[j] > sl[swap]{
-                swap = j;
-            }else{seq = false}
+    fn sort_bubble(&self){
+        let mut i = self.arr_size-1;
+        let mut cpy = self.arr.to_vec();
+
+        if self.dec{
+            while i > 1{
+                let mut swap = 0;
+                let mut seq = true;
+                for j in 1..=i{
+                    if cpy[j] < cpy[swap]{
+                        swap = j;
+                    }else{seq = false}
+                }
+                if seq == true {break}
+                cpy.swap(swap, i);
+                i -= 1;
+            }
+        }else{
+            while i > 1{
+                let mut swap = 0;
+                let mut seq = true;
+                for j in 1..=i{
+                    if cpy[j] > cpy[swap]{
+                        swap = j;
+                    }else{seq = false}
+                }
+                if seq == true {break}
+                cpy.swap(swap, i);
+                i -= 1;
+            }
         }
-        if seq == true {break}
-        sl.swap(swap, i);
-        i -= 1;
+        
+        println!("{:?}", cpy);
+        if self.dec  {self.dec_test(&mut cpy);}
+        else    {self.inc_test(&mut cpy);}
+    }
+
+    fn sort_bubblev2(&self){
+        let n = self.arr_size;
+        let mut cpy = self.arr.to_vec();
+
+        if self.dec{
+            'outer: for i in 0..n-1{
+                let mut seq = true;
+                for j in 0..n-i-1{
+                    if cpy[j] < cpy[j+1] {
+                        cpy.swap(j, j+1);
+                        seq = false;
+                    }
+                }
+                if seq {break 'outer}
+            }
+        }else{
+            'outer: for i in 0..n-1{
+                let mut seq = true;
+                for j in 0..n-i-1{
+                    if cpy[j]>cpy[j+1] {
+                        cpy.swap(j, j+1);
+                        seq = false;
+                    }
+                }
+                if seq {break 'outer}
+            }
+        }
+        
+        println!("{:?}", cpy);
+        if self.dec  {self.dec_test(&cpy);}
+        else    {self.inc_test(&cpy);}
+    }
+
+    fn dec_test(&self, v: & Vec<usize>)->bool{
+        for i in 0..self.arr_size-1{
+            if v[i] < v[i+1] && v[i] != v[i+1] {
+                println!("!! array sorted incorrectly (decreasing) !!");
+                return false
+            }
+        }
+        println!("array sorted correctly (decreasing)");
+        true
+    }
+
+    fn inc_test(&self, v: & Vec<usize>)->bool{
+        for i in 0..self.arr_size-1{
+            if v[i] > v[i+1] && v[i] != v[i+1] { 
+                println!("!! array sorted incorrectly (increasing) [{}] !!", v[i]);
+                return false
+            }
+        }
+        println!("array sorted correctly (increasing)");
+        true
+    }
+
+    fn arr_gen(&mut self){
+        let mut gen = rand::thread_rng();
+        for _ in 0..self.arr_size{
+            &self.arr.push(gen.gen_range(0..=self.arr_range));
+        }
     }
 }
-
-fn sort_bubblev2<T: Ord>(sl: &mut [T]){
-    let n = sl.len();
-    for i in 0..n-1{
-        for j in 0..n-i-1{
-            if sl[j]>sl[j+1] {sl.swap(j, j+1)}
-        }
-    }
+struct sortx{
+    arr_size: usize,
+    arr_range: usize,
+    arr: Vec<usize>,
+    dec: bool
 }
 
 fn parse_int(msg: &str)->usize{
